f88170787980a516f73a05dac1e9e181172c295c
mandeepdhakal111 <mandeepdhakal11@gmail.com>
2020-11-07 11:12:49 +0545

    add employee successful

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using Employee_Management.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Employee_Management.Controllers
+{
+    public class EmployeeController : Controller
+
+    {
+        employee_dbEntities dbObj = new employee_dbEntities();
+        // GET: Employee
+        public ActionResult Employee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddEmployee(employee_table model)
+        {
+            employee_table obj = new employee_table();
+            obj.name = model.name;
+            obj.email = model.email;
+            obj.phone = model.phone;
+            obj.address = model.address;
+            obj.description = model.description;
+
+            dbObj.employee_table.Add(obj);
+            dbObj.SaveChanges();
+            return View(viewName: "Employee");
+        }
+    }
+}
